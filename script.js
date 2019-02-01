076971ecb16732b38778611c753096681a9f7232
ApexWilson <44215785+ApexWilson@users.noreply.github.com>
2019-01-31 21:58:24 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#towering").click(function(){
+    $("#tornado").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#sylph").click(function(){
+    $("#breath").hide();
+});
+
+
+
+//Spell #3
+
+$("#shower").click(function(){
+    $("#blades").slideUp();
+});
+
+
+
